2e64e25776fd5f50c06dab79568af220390d1ca2
Adriano Silva <adrianon10@gmail.com>
2024-04-10 04:22:02 -0300

    feat: add helper method findEventById

--- backend/src/contracts/event-repository.ts
+++ backend/src/contracts/event-repository.ts
@@ -1,8 +1,11 @@
 import { type Event } from '@prisma/client'
 import { type ICreateEvent } from '../dtos/create-event'
+import { type GetEventValidationResponse } from '../validations/get-event-validation'
 
 export interface IEventRepository {
   create: (data: ICreateEvent) => Promise<Event>
-  findEventById: (event_id: string) => Promise<Event | null>
+  findEventById: (
+    event_id: string,
+  ) => Promise<GetEventValidationResponse | null>
   findEventBySlug: (slug: string) => Promise<Event | null>
 }
--- backend/src/repositories/events-repository.ts
+++ backend/src/repositories/events-repository.ts
@@ -3,12 +3,27 @@ import { type Event } from '@prisma/client'
 import { type IEventRepository } from '../contracts/event-repository'
 import { type ICreateEvent } from '../dtos/create-event'
 import { prisma } from '../lib/prisma'
+import { type GetEventValidationResponseRepository } from '../validations/get-event-validation'
 
 export class EventRepository implements IEventRepository {
-  public async findEventById(event_id: string): Promise<Event | null> {
+  public async findEventById(
+    event_id: string,
+  ): Promise<GetEventValidationResponseRepository | null> {
     const event = await prisma.event.findUnique({
       where: {
         id: event_id,
+      },
+      select: {
+        id: true,
+        title: true,
+        slug: true,
+        details: true,
+        maximum_attendees: true,
+        _count: {
+          select: {
+            attendees: true,
+          },
+        },
       },
     })
 
