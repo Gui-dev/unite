5312d3f8509142a66839ae7f11346fba81515010
Adriano Silva <adrianon10@gmail.com>
2024-04-11 03:35:30 -0300

    feat: add get attendee badge controller

---
+++ backend/src/controllers/get-attendee-badge-controller.ts
@@ -0,0 +1,16 @@
+import { type FastifyReply, type FastifyRequest } from 'fastify'
+import { getAttendeeBadgeValidation } from '../validations/get-attendee-badge-validation'
+import { GetAttendeeBadgeUseCase } from '../use-case/get-attendee-badge-use-case'
+
+export class GetAttendeeBadgeController {
+  public async show(
+    request: FastifyRequest,
+    reply: FastifyReply,
+  ): Promise<FastifyReply> {
+    const { attendee_id } = getAttendeeBadgeValidation.parse(request.params)
+    const getAttendeeBadgeUseCase = new GetAttendeeBadgeUseCase()
+    const attendee = await getAttendeeBadgeUseCase.execute(attendee_id)
+
+    return reply.status(200).send({ attendee })
+  }
+}
