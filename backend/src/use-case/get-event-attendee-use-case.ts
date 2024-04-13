a98c42854c005b0b7b6a08fe68787236f5206562
Adriano Silva <adrianon10@gmail.com>
2024-04-13 04:05:32 -0300

    feat: add get event attendee use case

---
+++ backend/src/use-case/get-event-attendee-use-case.ts
@@ -0,0 +1,38 @@
+import { type IAttendeesRepositoryContract } from '../contracts/attendees-repository-contract'
+import { AttendeesRepository } from '../repositories/attendees-repository'
+import { type IGetEventAttendeesDTO } from '../dtos/get-event-attendees-DTO'
+import { type GetEventAttendeesValidationResponseUseCase } from '../validations/get-event-attendees-validation'
+
+export class GetEvenAttendeeUsecase {
+  public readonly attendees_repository: IAttendeesRepositoryContract
+
+  constructor() {
+    this.attendees_repository = new AttendeesRepository()
+  }
+
+  public async execute({
+    event_id,
+    query,
+    page_index,
+  }: IGetEventAttendeesDTO): Promise<
+    GetEventAttendeesValidationResponseUseCase[]
+  > {
+    const attendees_result = await this.attendees_repository.find({
+      event_id,
+      query,
+      page_index,
+    })
+
+    const attendees = attendees_result.map((attendee) => {
+      return {
+        id: attendee.id,
+        name: attendee.name,
+        email: attendee.email,
+        created_at: attendee.created_at,
+        check_in_at: attendee.check_in?.created_at ?? null,
+      }
+    })
+
+    return attendees
+  }
+}
