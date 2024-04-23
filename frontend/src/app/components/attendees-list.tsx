3ac8d0b8c63de2494d8e9b219e9c31f2644dd564
Adriano Silva <adrianon10@gmail.com>
2024-04-23 03:36:37 -0300

    feat: add prisma seed and attendees count

--- frontend/src/app/components/attendees-list.tsx
+++
@@ -1,7 +0,0 @@
-export const AttendeesList = () => {
-  return (
-    <div>
-      <h1>Attendees</h1>
-    </div>
-  )
-}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,9 @@
 import { Header } from '@/components/header'
-import { AttendeesList } from './components/attendees-list'
+import { AttendeesList } from '@/components/attendees-list'
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-2 max-w-[1216px] mx-auto py-5">
+    <div className="flex flex-col gap-5 max-w-[1216px] mx-auto py-5">
       <Header />
       <AttendeesList />
     </div>
